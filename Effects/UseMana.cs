d134713d48865d7da68454ffb051e0bec764375d
Eivind <ehaugw@gmail.com>
2022-12-18 14:09:06 +0100

    implement UseMana effect

---
+++ Effects/UseMana.cs
@@ -0,0 +1,20 @@
+﻿namespace TinyHelper
+{
+    using SideLoader;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using UnityEngine;
+
+
+    public class UseMana : Effect
+    {
+
+        public float UsedMana = 0;
+
+        protected override void ActivateLocally(Character _affectedCharacter, object[] _infos)
+        {
+            _affectedCharacter.Stats.UseMana(null, UsedMana);
+        }
+    }
+}
