df2c10cea7e91b0bf7a16113b5c2d4cbffdbd91f
Eivind <ehaugw@gmail.com>
2022-12-19 19:07:10 +0100

    implement EquipSkillDurabilityCondition

---
+++ Effects/EquipSkillDurabilityCondition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace TinyHelper
+{
+    public class EquipSkillDurabilityCondition : EquipDurabilityCondition
+    {
+        protected override bool CheckIsValid(Character _affectedCharacter)
+        {
+            if (transform.parent.parent.gameObject.GetComponent<Skill>() is Skill skill)
+            {
+                DurabilityRequired = skill.DurabilityCost;
+            }
+            return base.CheckIsValid(_affectedCharacter);
+        }
+
+        public static Skill.ActivationCondition AddToSkill(Skill skill, EquipmentSlot.EquipmentSlotIDs slot)
+        {
+            var activationConditions = TinyGameObjectManager.GetOrMake(skill.transform, "AdditionalActivationConditions", true, true);
+            var gameObj = TinyGameObjectManager.GetOrMake(activationConditions, "EquipSkillDurabilityCondition", true, true).gameObject;
+
+            var condition = new Skill.ActivationCondition();
+            var myCondition = gameObj.AddComponent<EquipSkillDurabilityCondition>();
+            myCondition.EquipmentSlot = slot;
+
+            condition.Condition = myCondition;
+
+            At.SetValue("Your left hand equipement is too damaged to be used this way.", typeof(Skill.ActivationCondition), condition, "m_defaultMessage");
+
+            List<Skill.ActivationCondition> conditions = ((At.GetValue(typeof(Skill), skill, "m_additionalConditions") as Skill.ActivationCondition[])?.ToList()) ?? new List<Skill.ActivationCondition>();
+            conditions.Add(condition);
+            At.SetValue(conditions.ToArray(), typeof(Skill), skill, "m_additionalConditions");
+            return condition;
+        }
+    }
+}
