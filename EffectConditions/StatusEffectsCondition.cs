988a41f5c79d8e468b2fb81f5f05684f868a298b
Eivind <ehaugw@gmail.com>
2022-12-21 01:24:09 +0100

    implement StatusEffectsCondition

---
+++ EffectConditions/StatusEffectsCondition.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace TinyHelper
+{
+    public class StatusEffectsCondition : EffectCondition
+    {
+        public string[] StatusEffectNames;
+        public enum LogicType
+        {
+            Any,
+            All
+        };
+        public LogicType Logic = LogicType.Any;
+
+        protected override bool CheckIsValid(Character _affectedCharacter)
+        {
+            if (_affectedCharacter?.StatusEffectMngr is StatusEffectManager statusEffectManager)
+            {
+                var bools = StatusEffectNames.Select(x => statusEffectManager.GetStatusEffectOfName(x) != null);
+                return (Logic == LogicType.Any ? bools.Any(x => x) : bools.All(x => x));
+
+            }
+            return false;
+        }
+    }
+}
