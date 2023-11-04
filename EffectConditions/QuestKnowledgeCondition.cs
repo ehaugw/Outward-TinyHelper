db40c7975328bd5f26f6920d260b0c40697a7e92
Eivind <ehaugw@gmail.com>
2023-11-04 23:54:23 +0100

    more streamlined HasQuestKnowledge

--- EffectConditions/QuestKnowledgeCondition.cs
+++ EffectConditions/QuestKnowledgeCondition.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using NodeCanvas.BehaviourTrees;
+using System.Linq;
 using UnityEngine;
 
 namespace TinyHelper
@@ -6,21 +7,11 @@ namespace TinyHelper
     public class QuestKnowledgeCondition : EffectCondition
     {
         public int[] Quests;
-        public enum LogicType
-        {
-            Any,
-            All
-        };
         public LogicType Logic = LogicType.Any;
-
+        
         protected override bool CheckIsValid(Character _affectedCharacter)
         {
-            if (_affectedCharacter?.Inventory?.QuestKnowledge is CharacterQuestKnowledge q)
-            {
-                var bools = Quests.Select(x => q.IsItemLearned(x));
-                return (Logic == LogicType.Any ? bools.Any(x => x) : bools.All(x => x));
-            }
-            return false;
+            return QuestRequirements.HasQuestKnowledge(_affectedCharacter, Quests, Logic);
         }
     }
 }
---
+++ QuestRequirements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyHelper
+{
+    public class QuestRequirements
+    {
+        public static bool HasQuestKnowledge(Character character, int[] questIDs, LogicType logicType, bool inverted=false)
+        {
+            bool result = false;
+            if (character?.Inventory?.QuestKnowledge is CharacterQuestKnowledge q)
+            {
+                var bools = questIDs.Select(x => q.IsItemLearned(x));
+                result = result || (logicType == LogicType.Any ? bools.Any(x => x) : bools.All(x => x));
+            }
+            return result ^ inverted;
+        }
+    }
+}
