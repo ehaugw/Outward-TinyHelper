c633e33dbac6b89b9a428e5fd349d564950e451a
Eivind <ehaugw@gmail.com>
2023-11-25 14:49:36 +0100

    Add completed/not completed flag to QuestRequirements

--- QuestRequirements.cs
+++ QuestRequirements.cs
@@ -8,12 +8,12 @@ namespace TinyHelper
 {
     public class QuestRequirements
     {
-        public static bool HasQuestKnowledge(Character character, int[] questIDs, LogicType logicType, bool inverted=false)
+        public static bool HasQuestKnowledge(Character character, int[] questIDs, LogicType logicType, bool inverted=false, bool requireCompleted=false)
         {
             bool result = false;
             if (character?.Inventory?.QuestKnowledge is CharacterQuestKnowledge q)
             {
-                var bools = questIDs.Select(x => q.IsItemLearned(x));
+                var bools = questIDs.Select(x => q.IsItemLearned(x) && (q.IsQuestCompleted(x) || !requireCompleted));
                 result = result || (logicType == LogicType.Any ? bools.Any(x => x) : bools.All(x => x));
             }
             return result ^ inverted;
