dca4997bb7318eb82bdaeabaefc4588bba054524
Eivind <ehaugw@gmail.com>
2024-07-11 10:47:11 +0200

    implement aniumation extension

---
+++ Extensions/AnimatorExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TinyHelper
+{
+    public static class AnimatorExtension
+    {
+        public static bool HasParameter(this Animator animator, string paramName)
+        {
+            foreach (AnimatorControllerParameter param in animator.parameters)
+            {
+                if (param.name == paramName)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
