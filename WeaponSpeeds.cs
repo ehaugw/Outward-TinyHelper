01513c3e819d9d7757f13cc5b176f0e7ff55412f
Eivind <ehaugw@gmail.com>
2024-07-16 14:29:03 +0200

    define HeavyImpactModifiers

--- WeaponSpeeds.cs
+++ WeaponSpeeds.cs
@@ -18,5 +18,17 @@ namespace TinyHelper
             { Weapon.WeaponType.Spear_2H,   1.499f},
             { Weapon.WeaponType.Halberd_2H, 1.612f}
         };
+
+        public static Dictionary<Weapon.WeaponType, float> HeavyImpactModifiers = new Dictionary<Weapon.WeaponType, float>
+        {
+            { Weapon.WeaponType.Sword_1H,   1.3f},
+            { Weapon.WeaponType.Axe_1H,     1.3f},
+            { Weapon.WeaponType.Mace_1H,    2.5f},
+            { Weapon.WeaponType.Sword_2H,   1.5f},
+            { Weapon.WeaponType.Axe_2H,     1.3f},
+            { Weapon.WeaponType.Mace_2H,    2.0f},
+            { Weapon.WeaponType.Spear_2H,   1.2f},
+            { Weapon.WeaponType.Halberd_2H, 1.3f}
+        };
     }
 }
