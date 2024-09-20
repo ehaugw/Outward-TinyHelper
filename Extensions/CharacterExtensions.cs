2746383b1737aa9ba07b074fabcbc621fdce4f84
Eivind <ehaugw@gmail.com>
2024-09-20 12:45:37 +0200

    fix EquippedOnBag to find items rather than just equipment

--- Extensions/CharacterExtensions.cs
+++ Extensions/CharacterExtensions.cs
@@ -9,7 +9,7 @@ namespace CharacterExtensions
     {
         public static List<Item> EquippedOnBag(this Character character)
         {
-            return character?.Inventory?.GetOwnedItems(TinyTagManager.GetOrMakeTag(IDs.EquipmentTag))?.Where(x => x.DisplayedOnBag)?.ToList();
+            return character?.Inventory?.GetOwnedItems(TinyTagManager.GetOrMakeTag(IDs.ItemTag))?.Where(x => x.DisplayedOnBag)?.ToList();
         }
     }
 }
