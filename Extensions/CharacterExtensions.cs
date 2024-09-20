3c738e29157c94d1ac1c557dfa71de950acb9f7e
Eivind <ehaugw@gmail.com>
2024-09-20 12:30:15 +0200

    implement character extesions with EquippedOnBag

---
+++ Extensions/CharacterExtensions.cs
@@ -0,0 +1,15 @@
+﻿using InstanceIDs;
+using System.Collections.Generic;
+using System.Linq;
+using TinyHelper;
+
+namespace CharacterExtensions
+{
+    public static class CharacterExtensions
+    {
+        public static List<Item> EquippedOnBag(this Character character)
+        {
+            return character?.Inventory?.GetOwnedItems(TinyTagManager.GetOrMakeTag(IDs.EquipmentTag))?.Where(x => x.DisplayedOnBag)?.ToList();
+        }
+    }
+}
