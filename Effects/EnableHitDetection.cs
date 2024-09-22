29c2ed06759ff6254ed9fd7a53ba7cc6e8c819c9
Eivind <ehaugw@gmail.com>
2024-09-22 17:15:58 +0200

    implement EnableHitDetection from CustomWeaponBehaviour

---
+++ Effects/EnableHitDetection.cs
@@ -0,0 +1,17 @@
+﻿namespace TinyHelper
+{
+    public class EnableHitDetection : Effect
+    {
+        protected override void ActivateLocally(Character _affectedCharacter, object[] _infos)
+        {
+            if (ParentItem is MeleeSkill meleeSkill && meleeSkill.MeleeHitDetector != null && _affectedCharacter?.SkillMeleeDetector is MeleeHitDetector meleeHitDetector)
+            {
+                meleeHitDetector.HitStarted(-1);
+            }
+            else if (_affectedCharacter?.CurrentWeapon is MeleeWeapon meleeWeapon)
+            {
+                meleeWeapon.HitStarted(-1);
+            }
+        }
+    }
+}
